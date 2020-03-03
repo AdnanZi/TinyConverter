13ec8ae4a1f48697663cbdd596f56d085e5de2e7
Adnan Zildzic <adnan.zildzic@gmail.com>
2020-03-03 11:58:33 +0100

    Integrated CocoaPods.

---
+++ TinyConverter/TinyConverter/IoCConfigurator.swift
@@ -0,0 +1,12 @@
+//
+//  IoCConfigurator.swift
+//  TinyConverter
+//
+//  Created by Adnan Zildzic on 03.03.20.
+//  Copyright © 2020 Adnan Zildzic. All rights reserved.
+//
+import UIKit
+
+class IoCConfigurator {
+
+}
